2c77bd228f097aaacc823eaa705c4c1d0067c4c0
paul <paulmarketandtech@gmail.com>
2026-06-24 09:11:31 +0200

    creating a roadmap

---
+++ weekly_tickers_update/planning.py
@@ -0,0 +1,30 @@
+"""
+WORKFLOW:
+0. Once a month (at the end) dowload manually spreadsheet from website https://www.nasdaq.com/market-activity/stocks/screener
+  - create a subfolder where to store those files
+  - filter MC > $100M (number is still open)
+    - populate table 'all_tickers' (?)
+  - setup crontab to run it every first of the month (?)
+
+1. Every Sunday run a script which reads all tickers from table 'all_tickers'
+and dowloads MC from yfinance and updates the existing one.
+"""
+
+"""
+File structure and funcions in it
+monthly_update_from_file.py 
+* filter_above_given_MC(MC, filename) -> df (?) 
+    - reads filename, filters above MC and returns df 
+* update_DB_with_tickers_and_MC(df) -> void (?)
+    - sort df via MC (?)
+    - delete everything from a table 
+    - update with new values
+
+
+yf_weekly_tickers_update.py 
+(happens only on Sundays)
+* create_list_of_tickers() -> df 
+    - reads data from DB, creates and returns df 
+* update_MC_from_YF(df) -> void 
+    - loops through all tickers and dowloads MC from YF 
+"""
